3c2d13a53585bfb3ddc2b10a5cd23ed3cb65381c
Nikita Kartashov <snailandmail@gmail.com>
2018-05-09 23:16:39 +0100

    Add download buttons with interval

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,13 +1,34 @@
-$(document).ready(() => {
-  var image_url = chrome.extension.getURL('images/download-button.svg');
-  var r = $('<input/>').attr({
-    type: "button",
-  });
-  r.css("background-image", "url('" + image_url + "')");
-  r.css("background", "url('" + image_url + "')");
-  r.css("display", "inline-block");
-  r.css("background-size", "100%");
-  r.css("height", '16px');
-  r.css("width", '16px');
-  $('div.ProfileTweet-actionList').append(r);
-})
+var Downloader = {
+  DOWNLOAD_BUTTON_URL: chrome.extension.getURL('images/download-button.svg'),
+  DOWNLOADER_CLASS: "has-downloader-button",
+
+  init: function() {
+    setInterval(function() {
+      Downloader.injectButtons();
+    }, 2000);
+  },
+
+  buildDowloadButton: function(content_url) {
+    var result = $('<input/>').attr({
+      type: "button",
+    });
+    result.css("background-image", "url('" + Downloader.DOWNLOAD_BUTTON_URL + "')");
+    result.css("background", "url('" + Downloader.DOWNLOAD_BUTTON_URL + "')");
+    result.css("display", "inline-block");
+    result.css("background-size", "100%");
+    result.css("height", '16px');
+    result.css("width", '16px');
+    return result;
+  },
+
+  injectButtons: function() {
+    var parentsToUpdate = $('div.tweet, .has-cards').not('.' + Downloader.DOWNLOADER_CLASS);
+    parentsToUpdate.addClass(Downloader.DOWNLOADER_CLASS);
+    var children = parentsToUpdate.find(
+      '.ProfileTweet-actionList'
+    );
+    children.append(Downloader.buildDowloadButton(''));
+  }
+};
+
+Downloader.init()

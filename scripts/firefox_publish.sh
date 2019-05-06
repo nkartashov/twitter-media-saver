9f1e348755021c24a8b9dabee3f62f887cdedf62
Nikita Kartashov <snailandmail@gmail.com>
2019-05-06 19:25:42 +0100

    Add id to firefox-publish

--- scripts/firefox_publish.sh
+++ scripts/firefox_publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx web-ext sign --api-key="${FF_JWT_ISSUER}" --api-secret="${FF_JWT_SECRET}" --channel=listed
+npx web-ext sign --id='{'"${APP_ID}"'}' --api-key="${FF_JWT_ISSUER}" --api-secret="${FF_JWT_SECRET}" --channel=listed
